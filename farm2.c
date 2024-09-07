5842ad37aaae4200cc49daa09b00b01da6d01181
luigiarena <luigiarena.cs@gmail.com>
2024-09-07 09:58:15 +0200

    Definite macro per i valori di default delle variabili n, q e t

--- farm2.c
+++ farm2.c
@@ -1,4 +1,13 @@
-#include "utility.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "masterworker.h"
+#include "collector.h"
+
+#define NTHREAD_DEFAULT 4
+#define QLEN_DEFAULT	8
+#define TDELAY_DEFAULT	0
 
 int main(int argc, char *argv[]){
 
