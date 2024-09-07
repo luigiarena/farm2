b7ef14a14b704b77ed1658543a3232ed60c0e29c
luigiarena <luigiarena.cs@gmail.com>
2024-09-07 11:07:56 +0200

    Aggiunte define per il numero minimo di thread e la lunghezza massima del path della directory

--- farm2.c
+++ farm2.c
@@ -2,18 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <getopt.h>
 #include "masterworker.h"
 #include "collector.h"
 
-#define NTHREAD_DEFAULT 4
-#define QLEN_DEFAULT	8
-#define TDELAY_DEFAULT	0
+#define MIN_NTHREAD			1
+#define PATHNAME_LEN	  255
+#define NTHREAD_DEFAULT 	4
+#define QLEN_DEFAULT		8
+#define TDELAY_DEFAULT		0
 
 int main(int argc, char *argv[]){
 
 	pid_t pid;
 	int nthread;
 	int qlen;
+
+    // Analizza gli argomenti dati in input -n
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+                num_threads = atoi(optarg);
+                if (num_threads <= 0) {
+                    fprintf(stderr, "Il numero di thread deve essere maggiore di 0.\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n num_threads]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
 	// Ignora SIGPIPE per tutti
 	signal(SIGPIPE, SIG_IGN);
