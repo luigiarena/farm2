8d65559bd89cdc7ab235407a32c51656186aac5a
luigiarena <luigiarena.cs@gmail.com>
2024-09-07 12:32:17 +0200

    Aggiunto getopt per gestire gli argomenti di input al programma

--- farm2.c
+++ farm2.c
@@ -6,27 +6,41 @@
 #include "masterworker.h"
 #include "collector.h"
 
-#define MIN_NTHREAD			1
-#define PATHNAME_LEN	  255
+#define NTHREAD_MIN			1
 #define NTHREAD_DEFAULT 	4
+#define PATHNAME_LEN	  255
 #define QLEN_DEFAULT		8
 #define TDELAY_DEFAULT		0
 
+// Funzione main per che lancia MasterWorker e fa partire il programma
 int main(int argc, char *argv[]){
 
+	// Dichiarazione e definizione delle variabili
 	pid_t pid;
-	int nthread;
-	int qlen;
+	int nthread = NTHREAD_DEFAULT;
+	int qlen = QLEN_DEFAULT;
+	int tdelay;
+	char *dirname;
 
     // Analizza gli argomenti dati in input -n
-    while ((opt = getopt(argc, argv, "n:")) != -1) {
+	int opt;
+    while ((opt = getopt(argc, argv, "n:q:d:t:")) != -1) {
         switch (opt) {
             case 'n':
-                num_threads = atoi(optarg);
-                if (num_threads <= 0) {
-                    fprintf(stderr, "Il numero di thread deve essere maggiore di 0.\n");
+                nthread = atoi(optarg);
+                if (nthread < NTHREAD_MIN) {
+                    fprintf(stderr, "Il numero di thread deve essere un numero intero maggiore o uguale ad 1 (default 4).\n");
                     exit(EXIT_FAILURE);
                 }
+                break;
+			case 'q':
+                qlen = atoi(optarg);
+                break;
+            case 't':
+                tdelay = atoi(optarg);
+                break;
+            case 'd':
+                dirname = optarg;
                 break;
             default:
                 fprintf(stderr, "Usage: %s [-n num_threads]\n", argv[0]);
