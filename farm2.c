03f9f226a2d7c4ab85ae9aaa68ddd46a5fa7b58b
luigiarena <luigiarena.cs@gmail.com>
2024-09-07 21:09:46 +0200

    Aggiunta dell'opzione -h ad getopt per la stampa completa di un messaggio di spiegazione dei parametri di input del programma

--- farm2.c
+++ farm2.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <getopt.h>
-
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "masterworker.h"
 #include "collector.h"
 
-#define NTHREAD_MIN			1
-#define NTHREAD_DEFAULT 	4
-#define QLEN_DEFAULT		8
-#define TDELAY_DEFAULT		0
-#define DNAME_DEFAULT	 NULL
-#define DNAME_PATHLEN	  255
+#define NTHREAD_MIN						1
+#define NTHREAD_DEFAULT 				4
+#define QLEN_DEFAULT					8
+#define TDELAY_DEFAULT					0
+#define DNAME_DEFAULT				 NULL
+#define DNAME_PATHLEN				  255
+#define SOCKETNAME			"./farm2.sck"
+#define SOCKETNAME_LEN				   11
 
-
+struct sockaddr_un sa;
 
 // Funzione main per che lancia MasterWorker e fa partire il programma
 int main(int argc, char *argv[]){
@@ -28,36 +34,49 @@ int main(int argc, char *argv[]){
 
     // Analizza i parametri dati in input -n
 	int opt;
-    while ((opt = getopt(argc, argv, "n:q:d:t:")) != -1) {
+    while ((opt = getopt(argc, argv, "hn:q:d:t:")) != -1) {
         switch (opt) {
+			case 'h':
+    			printf("Options:\n");
+    			printf("\t-n nthread\tnumero iniziale di thread worker\t(default 1)\n");
+    			printf("\t-q qlen\t\tlunghezza della coda concorrente\t(default 8)\n");
+    			printf("\t-t tdelay\tritardo inserimento task nella coda\t(default 0)\n");
+    			printf("\t-d dname\tnaviga nella directory per cercare\n"
+					   "\t\t\tfile da leggere in input\t\t(default .)\n");
+    			exit(EXIT_SUCCESS);
             case 'n':
                 nthread = atoi(optarg);
                 if (nthread < NTHREAD_MIN) {
-                    fprintf(stderr, "Il numero di thread deve essere un numero intero maggiore o uguale ad 1 (default 4).\n");
+                    fprintf(stderr, "Il numero di thread deve essere un numero intero maggiore o uguale ad %d\n", NTHREAD_MIN);
                     exit(EXIT_FAILURE);
                 }
                 break;
 			case 'q':
                 qlen = atoi(optarg);
+				if (qlen < 1) {
+                    fprintf(stderr, "La lunghezza della cosa deve essere un numero intero maggiore o uguale ad 1\n");
+                    exit(EXIT_FAILURE);
+                }
                 break;
             case 't':
                 tdelay = atoi(optarg);
+	            if (tdelay < 0) {
+                    fprintf(stderr, "Il tempo di delay deve essere un numero intero maggiore o uguale ad 0\n");
+                    exit(EXIT_FAILURE);
+                }
                 break;
             case 'd':
                 dname = optarg;
                 break;
-            case ':': {
-                fprintf(stderr, "L'opzione '-%c' richiede un argomento.\n", optopt);
-                exit(EXIT_FAILURE);
-            } break;
-            case '?': {
-                fprintf(stderr, "L'opzione '-%c' non è riconosciuta.\n", optopt);
-            } break;
             default:
                 fprintf(stderr, "Usage: %s [-n nthread] [-q qlen] [-t tdelay] [-d dname] [-h]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
+
+	// Creo il file socket
+    sa.sun_family = AF_UNIX;
+    strncpy(sa.sun_path, SOCKETNAME, strlen(SOCKETNAME)+1);
 
 	// Ignora SIGPIPE per tutti
 	signal(SIGPIPE, SIG_IGN);
