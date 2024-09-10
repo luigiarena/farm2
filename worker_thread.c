ddd0038fc07d9f22dbf8c393929aa70d3f2cda8f
luigiarena <luigiarena.cs@gmail.com>
2024-09-10 18:03:53 +0200

    Creati file per gestire le funzioni dei worker_thread

---
+++ worker_thread.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "worker_thread.h"
+
+// Funzione eseguita da ogni worker thread
+void* worker_thread(void* arg) {
+    int id = *((int*)arg);
+    printf("Worker %d: Inizio esecuzione (thread id: %lu)\n", id, pthread_self());
+    while (1) {
+        printf("Worker %d: Eseguendo...\n", id);
+        sleep(5);
+    }
+    return NULL;
+}
---
+++ worker_thread.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+// Funzione eseguita da ogni worker thread
+void* worker_thread(void* arg);
