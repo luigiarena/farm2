18eda6eda33d5b4d31ef8839f8c1ba2a2d1a28e4
luigiarena <luigiarena.cs@gmail.com>
2025-01-08 16:35:13 +0100

    Getopt è stata sostituita da una funzione creata da me per una gestione più snella e veloce delle opzioni, comprensiva di creazione della struttura dati necessaria al funzionamento di masterworker

--- src/farm.c
+++ src/farm.c
@@ -5,6 +5,7 @@
     File: farm.c
     Descrizione: 
 */
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,9 +19,10 @@
 #include <sys/un.h>
 //#include <sys/types.h>
 #include <sys/wait.h>
-//#include <sys/stat.h>
+#include <sys/stat.h>
 
 #include "utility.h"
+#include "explorer.h"
 #include "masterworker.h"
 #include "collector.h"
 
@@ -32,95 +34,47 @@
 
 //#define len_verbose 20
 
-void usage_help(char *pname);
-int add_dir(const char *dname, char *ar[], int index);
-
-struct sockaddr_un sa;
 extern int verbose;
+
+master_data_t *read_opt(int argc, char *argv[]);
+void usage_help(char *pname);
 
 // Funzione main per che lancia MasterWorker e fa partire il programma
 int main(int argc, char *argv[]){
 
 	// Dichiarazione e definizione di default delle variabili
 	pid_t pid;
+    /*
 	int nthread = NTHREAD_DEFAULT;		// numero di threads
 	int qlen = QLEN_DEFAULT;			// lunghezza della coda concorrente
 	long tdelay = TDELAY_DEFAULT;		// tempo di ritardo nell'inserimento dei task
 	char *dname = NULL;		            // path della directory da visitare
+    */
 
     // Analizza i parametri dati in input
     verbose = 0;
-	int opt;
-    while ((opt = getopt(argc, argv, "hvn:q:d:t:")) != -1) {
-        switch (opt) {
-			case 'h':
-    			printf("Opzioni:\n"
-    				"\t-n nthread\tnumero iniziale di thread worker\t(default 1)\n"
-    				"\t-q qlen\t\tlunghezza della coda concorrente\t(default 8)\n"
-    				"\t-t tdelay\tritardo inserimento task nella coda\t(default 0)\n"
-    				"\t-d dname\tnaviga nella directory per cercare\n"
-						"\t\t\tfile da leggere in input\t\t(default .)\n");
-    			exit(EXIT_SUCCESS);
-            case 'v':
-                verbose = 1;
-                break;
-            case 'n':
-                nthread = atoi(optarg);
-                if (nthread < NTHREAD_MIN) {
-                    fprintf(stderr, "Il numero di thread deve essere un numero intero maggiore o uguale ad %d\n", NTHREAD_MIN);
-                    exit(EXIT_FAILURE);
-                }
-                break;
-			case 'q':
-                qlen = atoi(optarg);
-				if (qlen < 1) {
-                    fprintf(stderr, "La lunghezza della cosa deve essere un numero intero maggiore o uguale ad 1\n");
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 't':
-                tdelay = atol(optarg);
-	            if (tdelay < 0) {
-                    fprintf(stderr, "Il tempo di delay deve essere un numero intero maggiore o uguale ad 0\n");
-                    exit(EXIT_FAILURE);
-                }
-                break;
-            case 'd':
-                dname = optarg;
-                break;
-            default:
-            usage_help(argv[0]);
-                exit(EXIT_FAILURE);
-        }
-    }
+    master_data_t * data = read_opt(argc, argv);
 
-    V_PRINT_MSG(FARM, "apertura");
+    printf("TEST DI STAMPA MASTER DATA\n");
+    printf("nthread %d\n", data->nthread);
+    printf("qlen %d\n", data->qlen);
+    printf("tdelay %ld\n", data->tdelay);
+    printf("dname %s\n", data->dname);
+    printf("num_file %d\n", data->num_file);
+    printf("file_list:\n");
+    for(int i=0; i<data->num_file; i++) {
+        printf("  file %d %s\n", i+1, data->file_list[i]);
+    }
+    printf("\n");
 
     // Se non esistono argomenti e -d non è settato chiude
-	if (optind >= argc && dname == NULL) {
+	if (data->num_file < 1 && data->dname == NULL) {
 		fprintf(stderr, "Nessun argomento fornito al programma.\n");
         usage_help(argv[0]);
 		exit(EXIT_FAILURE);
 	}
-    
-    // Altrimenti inizializza un array dei file inseriti come argomenti
-    int num_file = argc-optind;
 
-    char *file_list[num_file];
-    int index;
-    for (index=0; index<num_file; index++) file_list[index] = malloc(PATH_MAX_LEN);
-
-    index = 0;
-    while (optind < argc) {
-        strncpy(file_list[index], argv[optind], PATH_MAX_LEN);
-        optind++;
-        index++;
-    }
-
-    // TEST
-    printf("Numero di argomenti: %d\n", num_file);
-
-    for(int i=0; i<num_file; i++) printf("file n.%d: %s\n", i, file_list[i]);
+    V_PRINT_MSG(FARM, "apertura");
 
 	// Creazione del processo figlio
     V_PRINT_MSG(FARM, "creo processo collector");
@@ -134,7 +88,8 @@ int main(int argc, char *argv[]){
 		// figlio: Collector
 
         V_PRINT_MSG(COLLECTOR, "avvio funzione main");
-		collector_main(tdelay);
+        // Correggere dati di input
+		collector_main(data->tdelay);
 	} else {
 		// padre: MasterWorker
 
@@ -142,7 +97,7 @@ int main(int argc, char *argv[]){
 		sleep(1);
 
         V_PRINT_MSG(FARM,"avvio processo main di masterworker");
-		masterWorker_main(file_list, num_file, nthread, qlen, tdelay, dname);
+		masterWorker_main(data);
 
         V_PRINT_MSG(MASTERWORKER,"attendo chiusura di collector");
         // Attendo la chiusura di Collector
@@ -157,4 +112,126 @@ int main(int argc, char *argv[]){
 // Stampa un messaggio di aiuto sull'uso del programma
 void usage_help(char* pname) {
     fprintf(stderr, "Usage: %s [-n nthread] [-q qlen] [-t tdelay] [-d dname] [-h] [file1 file2 ...]\n", pname);
+}
+
+master_data_t *read_opt(int argc, char *argv[]) {
+    master_data_t *data = malloc(sizeof(master_data_t));
+    ec_val(data, NULL, "Errore malloc master_data");
+
+    // Inizializza i valori della struttura di Masterworker
+    data->nthread = NTHREAD_DEFAULT;
+    data->qlen = QLEN_DEFAULT;
+    data->tdelay = TDELAY_DEFAULT;
+    data->dname = NULL;
+    data->num_file = 0;
+
+    // Strutture di appoggio per creare la lista dei file - argomento
+    int file_temp[argc];
+    int index_temp = 0;
+
+    int index = 1;
+    char *opt;
+    while (index < argc) {
+        opt = argv[index];
+        if (opt[0] == '-') {
+            switch (opt[1]) {
+                case 'h':
+                    printf("Opzioni:\n"
+                        "\t-n nthread\tnumero iniziale di thread worker\t(default 1)\n"
+                        "\t-q qlen\t\tlunghezza della coda concorrente\t(default 8)\n"
+                        "\t-t tdelay\tritardo inserimento task nella coda\t(default 0)\n"
+                        "\t-d dname\tnaviga nella directory per cercare\n"
+                            "\t\t\tfile da leggere in input\t\t(default .)\n");
+                    exit(EXIT_SUCCESS);
+                case 'v':
+                    verbose = 1;
+                    break;
+                case 'n':
+                    index++;
+                    if (index >= argc) {
+                        fprintf(stderr, "Valore di -n non valido\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    opt = argv[index];
+                    data->nthread = atoi(opt);
+                    if (data->nthread < NTHREAD_MIN) {
+                        fprintf(stderr, "Il numero di thread deve essere un numero intero maggiore o uguale ad %d\n", NTHREAD_MIN);
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 'q':
+                    index++;
+                    if (index >= argc) {
+                        fprintf(stderr, "Valore di -q non valido\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    opt = argv[index];
+                    data->qlen = atoi(opt);
+                    if (data->qlen < 1) {
+                        fprintf(stderr, "La lunghezza della coda deve essere un numero intero maggiore o uguale ad 1\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 't':
+                    index++;
+                    if (index >= argc) {
+                        fprintf(stderr, "Valore di -t non valido\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    opt = argv[index];
+                    data->tdelay = atol(opt);
+                    if (data->tdelay < 0) {
+                        fprintf(stderr, "Il tempo di delay deve essere un numero intero maggiore o uguale ad 0\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 'd':
+                    index++;
+                    if (index >= argc) {
+                        fprintf(stderr, "Valore di -d non valido\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    opt = argv[index];
+                    if(strlen(opt) > PATH_MAX_LEN) {
+                        fprintf(stderr,"Nome directory troppo lungo (MAX 255 caratteri).\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    data->dname = malloc(PATH_MAX_LEN);
+                    strncpy(data->dname, opt, strlen(opt));
+                    DIR *dir = opendir(data->dname);
+                    if (!dir) {
+                        fprintf(stderr, "La directory inserita non e' valida: %s\n", data->dname);
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                default:
+                    fprintf(stderr, "Opzione non riconosciuta: -%s\n", opt);
+                    usage_help(argv[0]);
+                    exit(EXIT_FAILURE);
+                    break;
+            }
+        } else {
+            if (fopen(opt, "r") == NULL) {
+                fprintf(stderr, "Argomento non valido: %s\n", opt);
+                exit(EXIT_FAILURE);
+            } else {
+                file_temp[index_temp] = index;
+                index_temp++;
+                data->num_file++;
+            }
+        }
+        index++;
+    }
+
+    // Inizializzo la lista dei file passati come argomenti
+    data->file_list = malloc(sizeof(char *)*data->num_file);
+    index = 0;
+
+    while (index < data->num_file) {
+        data->file_list[index] = malloc(PATH_MAX_LEN);
+        strncpy(data->file_list[index], argv[file_temp[index]], PATH_MAX_LEN);
+        index++;
+    }
+
+    return data;
 }
